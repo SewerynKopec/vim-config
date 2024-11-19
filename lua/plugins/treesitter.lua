f9e64c2230e4615f39d25050d0e6adb0348334d7
Seweryn Kopeć <sewerynkopec@beone.pl>
2024-11-19 18:29:33 +0100

    removing ftl from treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'json', 'yaml', 'css', 'typescript', 'kotlin', 'ftl' },    
+      ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'json', 'yaml', 'css', 'typescript', 'kotlin'},    
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = { enable = true },
