b31803c892e630edf361d2db17b20991db611328
Seweryn <sewerynpkopec@gmail.com>
2024-11-21 21:43:10 +0100

    adding fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,3 +1,10 @@
 return {
-
+    {
+        'tpope/vim-fugitive',
+        cmd = { "Git", "G", "Gdiffsplit", "Gread", "Gwrite", "Ggrep", "Gmove", "Gdelete" },
+        ft = { "fugitive" },
+        config = function()
+            -- Additional fugitive-specific configurations can go here
+        end,
+    },
 }
