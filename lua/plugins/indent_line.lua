352a4df6568cbc3be1d917ac94e74c971a2ef46f
Seweryn <sewerynpkopec@gmail.com>
2024-11-21 21:42:44 +0100

    removing indent line on dashboard

--- lua/plugins/indent_line.lua
+++ lua/plugins/indent_line.lua
@@ -27,7 +27,17 @@ return {
             vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
         end)
 
-        require("ibl").setup { indent = { highlight = highlight } }
+        require("ibl").setup {
+            indent = {
+                highlight = highlight
+            },
+            exclude = {
+                filetypes = {
+                    'dashboard'
+                }
+            }
+
+        }
     end
 },
 }
