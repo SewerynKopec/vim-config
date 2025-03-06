02c235da98b9c280ad2baad2650966adc73230ca
Seweryn Kopeć <sewerynkopec@beone.pl>
2025-03-06 16:03:37 +0100

    fixing typo for previer in multigrep

--- lua/plugins/telescope/multigrep.lua
+++ lua/plugins/telescope/multigrep.lua
@@ -33,7 +33,7 @@ local live_multigrep = function(opts)
                 { "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case" },
             }
         end,
-        entry_make = make_entry.gen_from_vimgrep(opts),
+        entry_maker = make_entry.gen_from_vimgrep(opts),
         cwd = opts.cwd,
     }
     pickers.new(opts, {
