c1f927c365ffd7ed167726280f9ec9a7b0e94cb8
Seweryn Kopeć <sewerynkopec@beone.pl>
2026-09-11 08:45:21 +0200

    adjusting ts and json LSPs

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -136,9 +136,11 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        jsonls = { },
-        yamlls= { },
-        -- tsserver = { },
+        jsonls = {
+            cmd = { "json-languageserver.cmd", "--stdio" }
+        },
+        yamlls = { },
+        ts_ls = { },
         cssls = { },
         -- clangd = {},
         -- gopls = {},
